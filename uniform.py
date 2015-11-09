1c1f3c769e1355929e5255a351dc262f0b29f3f0
chris reilly <reilly.christopher@gmail.com>
2015-11-08 19:48:38 -0600

    mixed signals, plotting histograms

--- uniform.py
+++ uniform.py
@@ -1,20 +1,27 @@
 import numpy as np
 import numpy.random as ran
 from matplotlib import pyplot as plt
-SAMPLES = 100
+SAMPLES = 1000
+MINS = [4.,9.]
 MAXES = [5.,10.]
 s = [
-    ran.uniform(0,max,SAMPLES) for max in MAXES
+    ran.uniform(min,max,SAMPLES) for min,max in zip(MINS,MAXES)
 ]
-for signal in s:
-    plt.plot(signal)
-    plt.show()
-plt.plot(*s)
-plt.show()
-
 MIXING_MATRIX = [
     [1.,1.],
     [1.,-1.]
 ]
+x = [
+    s[0] + s[1],
+    s[0] - s[1]
+]
+BINS = 30
+plt.hist(s[0],BINS,label='source 1')
+plt.hist(s[1],BINS,label='source 2')
+plt.hist(x[0],BINS,label='sum')
+plt.hist(x[1],BINS,label='difference')
+plt.legend()
+plt.show()
+
 
 
