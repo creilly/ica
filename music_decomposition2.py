c0941292baac351f37f4b399acffc675afa322a8
Dave <kamkard@gmail.com>
2015-12-06 18:52:08 -0600

    kinda working now

--- music_decomposition2.py
+++ music_decomposition2.py
@@ -5,7 +5,7 @@ import wave
 import struct
 import os
 
-samples = 195 # how many music samples per track to analyze (max 200000) current 195000
+samples = 195000 # how many music samples per track to analyze (max 200000) current 195000
 delta = 1.e-4 # criterion for convergence 1e-4
 bins = 500 # number of bins in histgrams
 sound_program = 'start' # program that plays wav files (change this for your os)
@@ -144,9 +144,6 @@ def F(x):
 def f(x):
     return (1. - np.square(x)) * np.exp(-1. * np.square(x) / 2.)
 
-# Problem here! Need to create individual guesses of dimensions then
-# add to complete components then do ling.eigh
-# there are as many components as there are dimensions
 
 for dimension in range(dimensions):
    old_guess = np.random.uniform(-1.,1.,dimensions)
@@ -155,16 +152,18 @@ for dimension in range(dimensions):
 s, u = np.linalg.eigh(np.dot(components, components.T))
 components = np.dot(np.dot(u * (1. / np.sqrt(s)), u.T), components)
 
-components = components/ np.linalg.norm(components)
+# components = components/ np.linalg.norm(components)
 
 iterations = 0
 
 while True:
+    
     iterations+=1
-
+    newComponent = np.zeros((dimensions,dimensions))
+    
     for dimension in range(dimensions):
         old_guess = components[dimension,:]
-
+        
         new_guess = np.average(
             whitened * F(
                 old_guess.transpose().dot(whitened)
@@ -175,25 +174,32 @@ while True:
                 old_guess.transpose().dot(whitened)
             )            
         ) * old_guess
+
+        #new_guess = new_guess / np.linalg.norm(new_guess)
+
         newComponent[dimension,:] = new_guess
 
+    # Symetric Decorrelation
     s, u = np.linalg.eigh(np.dot(newComponent, newComponent.T))
     newComponent = np.dot(np.dot(u * (1. / np.sqrt(s)), u.T), newComponent)
-    newComponent = newComponent/ np.linalg.norm(newComponent)
 
-   # compute difference between new and old guess
-    delta_pos = np.linalg.norm(newComponent - components)
+    # compute difference between new and old guess
+    delta_pos = np.linalg.norm(np.abs(newComponent) - np.abs(components))
 
     # compute difference between new and negative of old guess
-    delta_neg = np.linalg.norm(newComponent + components)
-    print newComponent, "\n", components
+    delta_neg = np.linalg.norm(np.abs(newComponent) + np.abs(components))
+
+    #print "OldS \n", components, "\n NewS \n", newComponent
+    lim = max(np.abs(np.abs(np.diag(newComponent.dot(components.T)))-1))
+
     # set new guess to be old guess of next loop
     components = newComponent
+
 
     # if old guess is "same" as new guess (i.e. within arbitrary negative sign) then we're done
     if delta_pos < delta or delta_neg < delta:
         break      
-    print '%d iterations' % (iterations), delta_pos, delta_neg
+    print '%d iterations' % (iterations), delta_pos, delta_neg, lim
 
 # compute extracted signals by applying extracted weights on whitened signal data
 extracted_signals = np.vstack(components.transpose()).dot(whitened)
